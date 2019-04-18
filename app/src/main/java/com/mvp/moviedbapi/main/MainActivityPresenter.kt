2999592fe87cd9d36f29ce7572a123a8abb3ae37
dinhtho <tho.nguyen@beesightsoft.com>
2019-04-18 14:07:06 +0700

    replace GlobalScope

--- app/src/main/java/com/mvp/moviedbapi/main/MainActivityPresenter.kt
+++ app/src/main/java/com/mvp/moviedbapi/main/MainActivityPresenter.kt
@@ -7,9 +7,10 @@ import com.mvp.moviedbapi.base.BasePresenter
 import com.mvp.moviedbapi.constants.Urls
 import com.mvp.moviedbapi.models.response.SearchResults
 import com.mvp.moviedbapi.network.service.movie.MovieServiceBuilder
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import rx.Subscriber
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
@@ -72,17 +73,20 @@ class MainActivityPresenter : BasePresenter<MainActivityView> {
             mView?.showToast(R.string.search_error_no_text)
         }
 
-        GlobalScope.launch(Dispatchers.Main){
-           val searchResults = MovieServiceBuilder().
-                   getMoviesCoroutine(Urls.MOVIEDB_API_KEY_VALUE, text, page).await()
-           Log.e(TAG, "onNext" + searchResults.results?.get(0)?.originalTitle)
-           mView?.hideLoading()
-           mView?.updateMovieAdapter(searchResults)
-           val nextButtonGone = searchResults.totalPages!! < 2 || searchResults.page == searchResults.totalPages
-           mView?.setUpOnNextPageButton(text, if (nextButtonGone) View.GONE else View.VISIBLE, searchResults.page!! + 1)
-       }
+        CoroutineScope(Dispatchers.IO).launch {
+            val searchResults = MovieServiceBuilder().getMoviesCoroutine(Urls.MOVIEDB_API_KEY_VALUE, text, page).await()
+            Log.e(TAG, "onNext" + searchResults.results?.get(0)?.originalTitle)
+
+            withContext(Dispatchers.Main) {
+                mView?.hideLoading()
+                mView?.updateMovieAdapter(searchResults)
+                val nextButtonGone = searchResults.totalPages!! < 2 ||
+                        searchResults.page == searchResults.totalPages
+                mView?.setUpOnNextPageButton(text,
+                        if (nextButtonGone) View.GONE else View.VISIBLE, searchResults.page!! + 1)
+
+            }
+        }
     }
-
-
 
 }
